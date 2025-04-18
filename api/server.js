eb1f608d6ea9b5efebe89cc96f235b4d9ffd0db2
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-18 09:17:41 +0100

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { router: usersRouter } = require('./api/users');
+const { router: authRouter } = require('./api/auth');
+const articlesRouter = require('./api/articles');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api', usersRouter);  // /signup and /login
+app.use('/api', authRouter);   // /auth
+app.use('/api', articlesRouter); // /articles
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
