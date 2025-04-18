ebe3a7678f519135b5acde0cf81fe7225a49b037
nouxinf <bingchilling1915@gmail.com>
2025-04-18 14:59:53 +0100

    initialised node, added w3.css

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { router: usersRouter } = require('./api/users');
 const { router: authRouter } = require('./api/auth');
-const articlesRouter = require('./api/articles');
+const articlesRouter = require('./api/artic');
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+app.use(express.static('static'));
 
 app.use('/api', usersRouter);  // /signup and /login
 app.use('/api', authRouter);   // /auth
