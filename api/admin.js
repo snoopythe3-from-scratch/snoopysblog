6f6d73ae49d323a4cd7b589d570a1d5f4e8e25e8
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-19 16:10:23 +0100

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -3,3 +3,9 @@ const router = express.Router();
 const { authCodes: authCodes } = require('./auth')
 
 let admins = [ kRxZy_kRxZy, Snoopythe3 ];
+
+router.post('/admin', (req, res) => {
+  const auth = req.headers.Authorisation;
+  const user = req.body.user;
+  const username = req.body.username;
+  // pls add how to do 'if auth in authCodes & username in admins. 
