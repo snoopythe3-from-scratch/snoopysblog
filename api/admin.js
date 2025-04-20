ae01a9429cea2880cf1c79d57a8b0a3f098a94de
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-04-20 08:08:11 +0100

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { authCodes: authCodes } = require('./auth')
+const { authCodes } = require('./auth');
 
-let admins = [ kRxZy_kRxZy, Snoopythe3 ];
+// List of admin usernames
+let admins = ['kRxZy_kRxZy', 'Snoopythe3'];
 
 router.post('/admin', (req, res) => {
-  const auth = req.headers.Authorisation;
-  const user = req.body.user;
+  const auth = req.headers.authorization;
   const username = req.body.username;
-  // pls add how to do 'if auth in authCodes & username in admins. 
+  const userToBeAdmin = req.body.usertobeadmin;
+
+  // Check if the requester is a valid admin with a valid auth code
+  if (authCodes.includes(auth) && admins.includes(username)) {
+    // Check if the user to be promoted is not already an admin
+    if (!admins.includes(userToBeAdmin)) {
+      admins.push(userToBeAdmin);
+      res.status(200).send(`${userToBeAdmin} has been added as an admin.`);
+    } else {
+      res.status(409).send(`${userToBeAdmin} is already an admin.`);
+    }
+  } else {
+    res.status(403).send('Forbidden: Unauthorized access');
+  }
+});
+
+module.exports = router;
