1cdbfc7b022ddf389e3f57c2c9f6aa3ee35396d4
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-07-31 10:02:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
+import CreateArticle from "./CreateArticle"; // <- Import the article creation component
 
-// I tested out adding all three css files and it gives the same results as just having this one, so i deleted the other ones
-import "./styles/index-revamp.css"
-// css just for articles
-import "./styles/article.css"
+import "./styles/index-revamp.css";
+import "./styles/article.css";
 
 function App() {
   return (
-    <>
-    <Header />
-    <MainContent />
-    <Footer />
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/articles/create" element={<CreateArticle />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
