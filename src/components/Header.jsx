72a2eaa69198b314a46cf161639074bf8a6119cc
nouxinf <bingchilling1915@gmail.com>
2025-07-31 11:05:53 +0100

    moving write an article button to the right and removing sign in button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,10 @@ export default function Header() {
           <Link to="/"><img src={TSC} alt="TSC Logo" /></Link>
           <Link to="/"><a href="/#">Home</a></Link>
           <Link to="/"><a href="/#articles-begin">Articles</a></Link>
-          <Link to="/articles/create"><a href="/#">Write an Article</a></Link>
           <a href="/#">About</a>
         </div>
         <div className="nav-right">
-          <a href="#">Sign in</a>
+          <Link to="/articles/create"><a href="/#">Write an Article</a></Link>
         </div>
       </div>
     </div>
