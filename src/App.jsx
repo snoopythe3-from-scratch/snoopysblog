55975111ac6d97624b8b9bf240afbf892c241646
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-07-31 10:14:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
-import CreateArticle from "./CreateArticle"; // <- Import the article creation component
+import CreateArticle from "./createArticles";
 
 import "./styles/index-revamp.css";
 import "./styles/article.css";
-
+// I added routing to make it easier
 function App() {
   return (
     <Router>
