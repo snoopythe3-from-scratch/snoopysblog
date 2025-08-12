22437b1597ca38fd52f0a1f3262271d8d59e065a
snoopythe3 <snoopythe3fans@hotmail.com>
2025-08-12 14:22:34 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function About() {
     return (
-        <>
         <div id="about-content">
         <h1>About Snoopy's Blog</h1>
         <p>I am a person who loves coding. <a href="https://github.com/snoopythe3-from-scratch">Github</a>, <a href="https://scratch.mit.edu/users/snoopythe3/">Scratch</a></p></li>
-        </ul>
 
         <p>This website is based off <a href="https://thescratchchannel.vercel.app/">The Scratch Channel</a>.</p>
         
