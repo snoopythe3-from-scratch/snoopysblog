b38668d90d43b547f9916a73642ad172c1554790
nouxinf <bingchilling1915@gmail.com>
2025-08-23 09:41:52 +0100

    fixed comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import {Link} from "react-router-dom";
 
 export default function Footer() {
   return (
-    <!-- START Formbricks Surveys -->
+    {/* start formbricks survey*/}
 <script type="text/javascript">
 !function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://app.formbricks.com/js/formbricks.umd.cjs",t.onload=function(){window.formbricks?window.formbricks.setup({environmentId:"cmen97vlbo8mkrj01dqdq88zx",appUrl:"https://app.formbricks.com"}):console.error("Formbricks library failed to load properly. The formbricks object is not available.");};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();
 </script>
-<!-- END Formbricks Surveys -->
+  {/* end formbricks survey*/}
     <footer>
       <div className="footer-main">
         <strong>The Scratch Channel</strong> is not affiliated with Scratch, the Scratch Team, or the Scratch Foundation.
