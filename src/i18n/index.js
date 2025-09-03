11034dd5f9e96a998aecc3f26d4ac8c022b03511
nouxinf <bingchilling1915@gmail.com>
2025-09-03 17:35:46 +0100

    i18n init

---
+++ src/i18n/index.js
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./en.json";
+import eo from "./eo.json"
+
+i18n.use(initReactI18next).use(LanguageDetector).init({
+    resources: {
+        en: { translation: en }, // English
+        eo: { translation: eo } // Esperanto
+    },
+    fallbackLng: "en",    // fallback if translation is missing
+    interpolation: {
+        escapeValue: false
+    },
+    detection: {
+        order: ["localStorage", "navigator"],
+        caches: ["localStorage"],
+    }
+});
+
+export default i18n;
