745da2b62004ffcb69cd5057c1bed60e3f713c6c
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2025-10-15 15:19:57 -0400

    changed some stuff blind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,20 +29,27 @@ export default function LoginPage() {
   };
 
   return (
+    <div className="login-container">
     <form onSubmit={handleSignIn}>
+      <div className="form-group">
       <input
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email"
+        className="form-input"
       />
+      </div><div className="form-group">
       <input
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
+        className="form-input"
       />
-      <button type="submit">{t("login.signin")}</button>
+      </div>
+      <button className="submit-button" type="submit">{t("login.signin")}</button>
     </form>
+    </div>
   );
 }
