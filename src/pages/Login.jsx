a0f0e689f185a967695df1343723f2295fe6ba50
#FreeSchlep Snoopy <snoopythe3fans@hotmail.com>
2025-10-16 16:13:55 +0000

    Added a sign in message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -38,27 +38,25 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="login-container">
-    <form onSubmit={handleSignIn}>
-      <div className="form-group">
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        className="form-input"
-      />
-      </div><div className="form-group">
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        className="form-input"
-      />
-      </div>
-      <button className="submit-button" type="submit">{t("login.signin")}</button>
-    </form>
-    </div>
+    <><center><h1>Hello! Sign in to TSC</h1></center><div className="login-container">
+      <form onSubmit={handleSignIn}>
+        <div className="form-group">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="form-input" />
+        </div><div className="form-group">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="form-input" />
+        </div>
+        <center><button className="submit-button" type="submit">{t("login.signin")}</button></center>
+      </form>
+    </div></>
   );
 }
