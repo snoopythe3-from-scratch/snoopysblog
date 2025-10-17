86c263ab069eba2a041f975ce6ca39ac59d08600
#FreeSchlep Snoopy <snoopythe3fans@hotmail.com>
2025-10-17 16:26:50 +0100

    Integrate Google Custom Search in Header component
    Added Google Custom Search script and div for search functionality.
    
    Signed-off-by: #FreeSchlep Snoopy <snoopythe3fans@hotmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,6 +28,9 @@ export default function Header() {
                     <Link to="/lang">{t("header.language")}</Link>
                     <Link to="/about">{t("header.about")}</Link>
                 </div>
+                <script async src="https://cse.google.com/cse.js?cx=777e81daa01814f83">
+                </script>
+                <div class="gcse-search"></div>
                 <div className="nav-right">
                     <Link to="/articles/create">{t("header.write")}</Link>
                     <button
